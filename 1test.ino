33840baa9633fd1925494a6a4c04f15494b79194
Sanyadoda <65733159+Sanyadoda@users.noreply.github.com>
2020-06-25 22:34:11 -0400

    Add files via upload

---
+++ 1test.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("285.6875,0.625,4.375");
+  delay(1000);
+  
+
+}
