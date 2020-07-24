5a9c7e0334e2547bea808e9529990eaf8ddeb994
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-07-24 19:30:47 -0400

    Update stream1080.bash

--- PiVideo/stream1080.bash
+++ PiVideo/stream1080.bash
@@ -9,6 +9,6 @@ cd ~/LIVE/segmented_videos
 # -t 100000 = video time limit is 100 seconds
 # -sg 5000 = video segments every 5 seconds
 
-raspivid -t 9000000 -w 1920 -h 1080 -fps 25 -b 1200000 -p 0,0,1920,1280 -n -sg 20000 -o video%04d.h264 -send | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:2431/}' :demux=h264
+raspivid -t 9000000 -w 1920 -h 1080 -fps 25 -b 1200000 -p 0,0,1920,1280 -sg 20000 -o video%04d.h264 -send | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:2431/}' :demux=h264
 
 # Stream can be accessed on another computer by opening the VLC media player, navigating to File, clicking on Open Network, and inputting rtsp://<raspberrypi_IPaddress>:2431/
