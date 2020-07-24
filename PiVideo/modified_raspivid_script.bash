c8dfd5e307bbc9748137925bb0cd788ee21be334
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-07-24 19:24:25 -0400

    Update modified_raspivid_script.bash

--- PiVideo/modified_raspivid_script.bash
+++ PiVideo/modified_raspivid_script.bash
@@ -9,6 +9,6 @@ cd ~/LIVE/segmented_videos
 # -t 100000 = video time limit is 100 seconds
 # -sg 5000 = video segments every 5 seconds
 
-raspivid -t 9000000 -w 1280 -h 960 -fps 25 -b 1200000 -p 0,0,1280,960 -n -sg 20000 -o video%04d.h264 -send | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:2431/}' :demux=h264
+raspivid -t 9000000 -w 1280 -h 720 -fps 25 -b 1200000 -p 0,0,1280,720 -n -sg 20000 -o video%04d.h264 -send | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:2431/}' :demux=h264
 
 # Stream can be accessed on another computer by opening the VLC media player, navigating to File, clicking on Open Network, and inputting rtsp://<raspberrypi_IPaddress>:2431/
