6757182d440e9aecff04ca8f6d9eade841d53cb3
rubenturner <rubenjturner123@gmail.com>
2020-07-26 21:09:54 -0400

    Finalized script for post-flight video processing.

--- PiVideo/video_processing.bash
+++ PiVideo/video_processing.bash
@@ -10,7 +10,7 @@ echo "Enter the number of segmented video clips recorded:"
 read numvid
 
 rm videoprocessing.txt
-cat >> videoprocessing.txt
+
 
 if [[ "$numvid" -lt "10" ]]
 then
@@ -24,48 +24,52 @@ then
     echo -n "finalvideo.mp4" >> videoprocessing.txt
 fi
 
-if [[ "$numvid" -ge "10" ]]
-then
-    if [[ "$numvid" -ge "100" ]]
-    then 
-        echo -n "MP4Box -add video0001.h264 " >> videoprocessing.txt
-        for (( i = 2; i <= 9; i++ ))
-            do
-            echo -n "-cat video000" >> videoprocessing.txt
-            echo -n "$i" >> videoprocessing.txt
-            echo -n  ".h264 " >> videoprocessing.txt
-        done
-        for (( i = 10; i <= 99; i++ ))
-            do
-            echo -n "-cat video00" >> videoprocessing.txt
-            echo -n "$i" >> videoprocessing.txt
-            echo -n ".h264 " >> videoprocessing.txt
-        done
-        for (( i = 100; i <= $numvid; i++ ))
-            do
-            echo -n "-cat video0" >> videoprocessing.txt
-            echo -n "$i" >> videoprocessing.txt
-            echo -n ".h264 " >> videoprocessing.txt
-        done
-        echo -n "finalvideo.mp4" >> videoprocessing.txt
-    fi
 
-    if [[ "$numvid" -lt "100" ]] && [[ "$numvid" -ge "10" ]]
-    then	
-        echo -n "MP4Box -add video0001.h264 " >> videoprocessing.txt
-        for (( i = 2; i < 10; i++ ))
-            do
-            echo -n "-cat video000" >> videoprocessing.txt
-            echo -n "$i" >> videoprocessing.txt
-            echo -n ".h264 " >> videoprocessing.txt
-        done
-        for (( i = 10; i <= $numvid; i++ ))
-            do 
-            echo -n "-cat video00" >> videoprocessing.txt
-            echo -n "$i" >> videoprocessing.txt
-            echo -n ".h264 " >> videoprocessing.txt
-        done
+if [[ "$numvid" -ge "100" ]]
+then 
+    echo -n "MP4Box -add video0001.h264 " >> videoprocessing.txt
+    for (( i = 2; i <= 9; i++ ))
+        do
+        echo -n "-cat video000" >> videoprocessing.txt
+        echo -n "$i" >> videoprocessing.txt
+        echo -n  ".h264 " >> videoprocessing.txt
+    done
+    for (( i = 10; i <= 99; i++ ))
+        do
+        echo -n "-cat video00" >> videoprocessing.txt
+        echo -n "$i" >> videoprocessing.txt
+        echo -n ".h264 " >> videoprocessing.txt
+    done
+    for (( i = 100; i <= $numvid; i++ ))
+        do
+        echo -n "-cat video0" >> videoprocessing.txt
+        echo -n "$i" >> videoprocessing.txt
+        echo -n ".h264 " >> videoprocessing.txt
+    done
+    echo -n "finalvideo.mp4" >> videoprocessing.txt
+fi
+
+
+if [[ "$numvid" -ge "10" ]] && [[ "$numvid" -lt "100" ]]
+then
+    
+    echo -n "MP4Box -add video0001.h264 " >> videoprocessing.txt
+    
+    for (( i = 2; i < 10; i++ ))
+        do
+        echo -n "-cat video000" >> videoprocessing.txt
+        echo -n "$i" >> videoprocessing.txt
+        echo -n ".h264 " >> videoprocessing.txt
+    done
+    for (( i = 10; i <= $numvid; i++ ))
+        do 
+        echo -n "-cat video00" >> videoprocessing.txt
+        echo -n "$i" >> videoprocessing.txt
+        echo -n ".h264 " >> videoprocessing.txt
+    done
+
     echo -n "finalvideo.mp4" >> videoprocessing.txt 
+
 fi
 
 $(sed -n '1p' videoprocessing.txt)
