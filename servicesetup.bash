9e400a7f5504a551e3a033729d7765d57478bd6f
rubenturner <rubenjturner123@gmail.com>
2020-08-06 18:29:05 -0400

    finished service setup script to make dependent video recording and data logging scripts executable, and to automatically enable and setup the boot services

--- servicesetup.bash
+++ servicesetup.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# After script is executed, run 'sudo systemctl status video.service' and then 'sudo systemctl status video.service' to ensure that the services are configured correctly.
+
 sudo sed -i 's/geteuid/getppid/' /usr/bin/vlc
 
 sudo cp video.service /etc/systemd/system
 
+cd 
+cd LIVE
+chmod +x data_logging.py
+
+cd PiVideo
+chmod +x stream1080.bash
+
 sudo systemctl enable video.service
+
+sudo systemctl enable data.service
+
