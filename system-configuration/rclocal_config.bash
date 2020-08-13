425cdf25e1c470d3abaed6082896ba7d251d5f08
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-08-12 23:03:59 -0400

    Update rclocal_config.bash

--- system-configuration/rclocal_config.bash
+++ system-configuration/rclocal_config.bash
@@ -3,8 +3,12 @@
 # Will make modifications to rc.local in order to configure the data and video services to run at boot
 
 cd
+cd LIVE
+cd system-configuration
 
 chmod +x servicesetup.bash
+
+cd
 
 sudo sed -i -e '/bash/d' /etc/rc.local
 sudo sed -i -e '/python/d' /etc/rc.local
