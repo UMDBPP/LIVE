05865eb5d3eabed255d2b627652aba696accb2a3
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-08-12 20:12:08 -0400

    downloads all dependencies and configures rc.local for script run upon system boot

--- sysconfig.bash
+++ sysconfig.bash
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+cd
 
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vlc
+sudo apt-get install cmake
+sudo apt install python3-pip
+pip3 install pyserial
+pip3 install python-csv
+sudo apt install -y gpac
+
+git clone https://github.com/UMDBPP/userland.git
+
+cd
+cd userland
+
+git checkout devel
+
+./buildme
+
+cd
+cd LIVE
+
+chmod +x servicesetup.bash
 
 cd
 
