82c02c4b8df72cca70abc731b22282126146f654
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-08-12 20:02:32 -0400

    Update sysconfig.bash

--- sysconfig.bash
+++ sysconfig.bash
@@ -4,6 +4,6 @@
 
 cd
 
-sed -i -e '/bash/d' /etc/rc.local
-sed -i -e '/python/d' /etc/rc.local
-sed -i -e '$i \sudo bash /home/pi/LIVE/servicesetup.bash &\n' /etc/rc.local
+sudo sed -i -e '/bash/d' /etc/rc.local
+sudo sed -i -e '/python/d' /etc/rc.local
+sudo sed -i -e '$i \sudo bash /home/pi/LIVE/servicesetup.bash &\n' /etc/rc.local
