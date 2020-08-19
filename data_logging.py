4cefc2cf48efb2bd76b66f8abbeac156d1d67b14
ncondzal <natalie.condzal@gmail.com>
2020-08-19 00:06:45 -0400

    path changes to correct data.csv location

--- data_logging.py
+++ data_logging.py
@@ -4,14 +4,20 @@ Using serial communication, logs all data from PIDcontroller.ino on Arduino onto
 
 # shebang for python3, reccomended to run this software with python 3.5.3
 # !/usr/bin/env python 3
-$ sudo mkdir /mnt/ramdisk    # storing the data to RAM
-$ sudo mount -t tmpfs none /mnt/ramdisk
+# $ sudo mkdir /mnt/ramdisk    # storing the data to RAM
+# $ sudo mount -t tmpfs none /mnt/ramdisk
 
 # needed modules
 import serial  # install with "pip3 install pyserial"
 import csv  # install with "pip3 install python-csv"
+import os
 
 if __name__ == '__main__':
+    os.system("sudo mkdir /mnt/ramdisk")
+    os.system("sudo mount -t tmpfs none /mnt/ramdisk")
+    os.system("sudo mount -t tmpfs none /mnt/ramdisk")
+    os.system("mkdir ~/LIVE/data_files")
+    os.system("cd ~/LIVE/data_files")
     # find correct device with "ls /dev/tty*"
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # initializes serial communciations in ser object
     ser.flush()  # flushes any I/O buffer to avoid sending incomplete data at start of serial communication
@@ -26,11 +32,11 @@ if __name__ == '__main__':
             #csvfile.close()
             i=0
             path="data"+ str(i)+ ".csv"
-            for i in range (10):
+            while True:
               if os.path.exists("data"+ str(i)+ ".csv")==False:  # to check if the file already exists
                 file = open("data"+ str(i)+ ".csv", "a", encoding='utf-8')
                 x=os.stat("data"+ str(i)+ ".csv").st_size
-                for x in range (0,3):     # to write 3 lines of data into a file
+                for x in range (0,5000):     # to write 3 lines of data into a file
                    obj=csv.writer(file)  # creates csv file object
                    obj.writerow(line)
                 file.close()
