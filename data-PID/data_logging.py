5f07ff86f8e13fcc49b40d1fdc06f5d4f1619e11
ncondzal <natalie.condzal@gmail.com>
2020-08-19 21:08:24 -0400

    temporarily commented out thermal cutoff

--- data-PID/data_logging.py
+++ data-PID/data_logging.py
@@ -17,9 +17,9 @@ if __name__ == '__main__':
             line=ser.readline()  # reads in all bytes of data
             line=line.decode('utf-8').split()  # decodes line with utf-8 and splits into list
             print(line)  # sanity check - prints each line to terminal
-            if line[5] != 'Temp_degC':
-                if int(line[5]) >= 82:
-                    os.system("sudo shutdown -h now")
+            # if line[5] != 'Temp_degC':
+                # if int(line[5]) >= 82:
+                    # os.system("sudo shutdown -h now")
             csvfile=open('/home/pi/LIVE/data.csv','a',newline='\n')  # opens csv file
             obj=csv.writer(csvfile)  # creates csv file object
             obj.writerow(line)  # writes each line to a new row in csv
