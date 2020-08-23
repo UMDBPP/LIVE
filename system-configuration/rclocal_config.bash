b7278a15533986db20f12ea4e21ef4be92358cbc
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-08-23 01:40:31 -0400

    fixed systemctl issue

--- system-configuration/rclocal_config.bash
+++ system-configuration/rclocal_config.bash
@@ -13,3 +13,5 @@ cd
 sudo sed -i -e '/bash/d' /etc/rc.local
 sudo sed -i -e '/python/d' /etc/rc.local
 sudo sed -i -e '$i \sudo bash /home/pi/LIVE/servicesetup.bash &\n' /etc/rc.local
+
+sudo systemctl daemon-reload
