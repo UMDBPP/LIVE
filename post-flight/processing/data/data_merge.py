76915c9a72ae7ddbda54fafb883ecd74b6415c09
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-10-02 16:10:47 -0400

    improved functionality update for testing

--- post-flight/processing/data/data_merge.py
+++ post-flight/processing/data/data_merge.py
@@ -1,12 +1,17 @@
-fout=open("out.csv","a")
-# first file:
-for line in open("log0.csv"):
-    fout.write(line)
-# now the rest:    
-for num in range(1,652):
-    f = open("log"+str(num)+".csv")
-    f.__next__() # skip the header
-    for line in f:
-         fout.write(line)
-    f.close() # not really needed
-fout.close()
+import os
+
+direc = input("Enter the absolute filepath that contains the segmented data files: ")
+if os.path.isdir(direc) == True:
+    numfiles = input("Enter the number of segmented files that need to be merged together: ")
+    fout=open("out.csv","a")
+    # first file:
+    for line in open("log0.csv"):
+        fout.write(line)
+    # now the rest:    
+    for num in range(1,652):
+        f = open("log"+str(num)+".csv")
+        f.__next__() # skip the header
+        for line in f:
+            fout.write(line)
+        f.close() # not really needed
+    fout.close()
