fcc20db7e7c43c896b1a9ada1094a27f4f251cc9
rubenturner <67194782+rubenturner@users.noreply.github.com>
2020-10-02 17:56:14 -0400

    second improved functionality update for testing

--- post-flight/processing/data/data_merge.py
+++ post-flight/processing/data/data_merge.py
@@ -1,14 +1,16 @@
 import os
 
+flightname = input("Enter the name of the flight: ")
 direc = input("Enter the absolute filepath that contains the segmented data files: ")
 if os.path.isdir(direc) == True:
     numfiles = input("Enter the number of segmented files that need to be merged together: ")
-    fout=open("out.csv","a")
+    mergedatafilename = "%sdata.csv" % flightname
+    fout=open(mergedatafilename,"a")
     # first file:
     for line in open("log0.csv"):
         fout.write(line)
     # now the rest:    
-    for num in range(1,652):
+    for num in range(1,numfiles):
         f = open("log"+str(num)+".csv")
         f.__next__() # skip the header
         for line in f:
