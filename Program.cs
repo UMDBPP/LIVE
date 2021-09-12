fbb32615b1339ae9088e443196f1cccfd6c1b654
rubenturner <67194782+rubenturner@users.noreply.github.com>
2021-09-12 11:17:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LIVE_Cube_Demonstration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
